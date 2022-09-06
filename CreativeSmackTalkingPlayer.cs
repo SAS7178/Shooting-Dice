62522b79dcb76e67866c87104686273d78e3122f
Stephen Smith <StephenASmith7178@gmail.com>
2022-09-06 15:32:29 -0400

    done

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
     public class CreativeSmackTalkingPlayer : Player
     {
-        
+        List<string> _TauntList = new List<string>
+        {
+            "Yo mother was a hamster",
+            "yo Daddy was reAAal short",
+            "welcome",
+        };
+
+        public void RandomTaunt()
+        {
+            int rnd = new Random().Next(_TauntList.Count);
+            Console.WriteLine(_TauntList[rnd]);
+        }
     }
 }
