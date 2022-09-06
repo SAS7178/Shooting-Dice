bcba6d249e8fc21ed51a851355f36c065ce875af
Stephen Smith <StephenASmith7178@gmail.com>
2022-09-06 15:14:52 -0400

    complete

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,17 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
-    // A player the prompts the user to enter a number for a roll
+    // A player that prompts the user to enter a number for a roll
     public class HumanPlayer : Player
     {
-        
+        public override int Roll()
+        {
+            Console.WriteLine("Enter a number of (1-6) for roll");
+            int chosenHumanRoll = int.Parse(Console.ReadLine());
+             return chosenHumanRoll;
+        }
     }
 }
